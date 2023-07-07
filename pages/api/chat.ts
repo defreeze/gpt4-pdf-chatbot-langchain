e81bfe5f0b053fc844fbc130c3e64d500dfdaf34
Alex de Vries <alexthevries@gmail.com>
2023-07-07 20:42:16 +0200

    code cleanup

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -4,13 +4,15 @@ import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { makeChain } from '@/utils/makechain';
 import { pinecone } from '@/utils/pinecone-client';
 import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
+import { generateQuery } from '@/utils/./cosmosDB-query-gen';
+import { executeGremlinQuery } from '@/utils/./gremlin-api';
+require('dotenv').config();
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   const { question, history } = req.body;
-
   console.log('question', question);
 
   //only accept post requests
@@ -18,47 +20,12 @@ export default async function handler(
     res.status(405).json({ error: 'Method not allowed' });
     return;
   }
-
   if (!question) {
     return res.status(400).json({ message: 'No question in the request' });
   }
+
   // OpenAI recommends replacing newlines with spaces for best results
   const sanitizedQuestion = question.trim().replaceAll('\n', ' ');
-
-
-
-  const gremlin = require('gremlin');
-  async function executeGremlinQuery(query: any) {
-    const endpoint = 'wss://plusdatabase.gremlin.cosmos.azure.com:443';
-    const key = 'Tn0VsSxCcH98TmcVpGzELezFsXHnpuWF4XcRgNiSoA1pOL59B1EDjkESKWuMOWqcQa4kZqtrFIAaACDblz5DGw==';
-    const database = 'websiteDEMO';
-    const container = 'webpages';
-    const authenticator = new gremlin.driver.auth.PlainTextSaslAuthenticator(`/dbs/${database}/colls/${container}`, key);
-
-    const client = new gremlin.driver.Client(
-      endpoint,
-      {
-        authenticator,
-        traversalsource: "g",
-        rejectUnauthorized: true,
-        mimeType: "application/vnd.gremlin-v2.0+json"
-      }
-    );
-
-    return new Promise((resolve, reject) => {
-      client.submit(query, {})
-        .then((result: any) => {
-          resolve(result.toArray());
-        })
-        .catch((error: any) => {
-          reject(error);
-        })
-        .finally(() => {
-          client.close();
-        });
-    });
-  }
-
 
   try {
     const index = pinecone.Index(PINECONE_INDEX_NAME);
@@ -69,7 +36,7 @@ export default async function handler(
       {
         pineconeIndex: index,
         textKey: 'text',
-        namespace: PINECONE_NAME_SPACE, //namespace comes from your config folder
+        namespace: PINECONE_NAME_SPACE, //namespace comes from the config folder
       },
     );
 
@@ -81,15 +48,10 @@ export default async function handler(
       chat_history: history || [],
     });
 
-    const query = `
-    g.V().hasLabel('webpages').has('webpages', 'UI Settings')
-    .repeat(bothE('button').otherV())
-    .until(hasLabel('webpages').has('webpages', 'GeniusPLUS launch'))
-    .path().by(valueMap('webpages')).by(valueMap('subLabel', 'label'))
-    .limit(1)
-  `;
+    const StartPage = 'UI Settings';
+    const GoalPage = 'GeniusPLUS launch';
+    const query = generateQuery(StartPage, GoalPage);
     const results: any = await executeGremlinQuery(query);
-    //console.log('Results from CosmosDB:', results);
 
     console.log('Individual Objects:');
     const objectsArray = results[0].objects;
