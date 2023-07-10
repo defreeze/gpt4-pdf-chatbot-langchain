d156d65d955395f60362a3d3c7bd955f53f436ec
Alex de Vries <alexthevries@gmail.com>
2023-07-10 16:19:25 +0200

    ready for demo

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -40,6 +40,7 @@ export default async function handler(
       },
     );
 
+
     //create chain
     const chain = makeChain(vectorStore);
     //Ask a question using chat history
@@ -48,10 +49,32 @@ export default async function handler(
       chat_history: history || [],
     });
 
-    var StartPage = 'overview';
-    var GoalPage = 'sdfsdf';
+    /*
+// method to extract and send metadata
+response.sourceDocuments.forEach((document: any, index: any) => {
+  const source = document.metadata.source;
+  console.log(`Metadata for Document ${index + 1}:`, source);
+});
+*/
+    const topDocument = response.sourceDocuments[0];
+    console.log('Top document:', topDocument);
 
-    const timeoutDuration = 2000; // Timeout duration in milliseconds
+    const Goalwebpage = topDocument.metadata.source.split('\\').pop().replace('.pdf', '');
+    console.log('Top webpage name:', Goalwebpage);
+
+
+    function extractPage(text: any) {
+      const match = text.match(/am at '(.*?)'/);
+      return match ? match[1] : null; // match[1] contains the value between the single quotes
+    }
+
+    const Alltxt = sanitizedQuestion;
+    console.log('all text:', Alltxt);
+
+    var StartPage = extractPage(Alltxt);
+    var GoalPage = Goalwebpage;
+    console.log('startpage:', StartPage);
+    const timeoutDuration = 2000;
     const query = generateQuery(StartPage, GoalPage);
     const executionPromise = executeGremlinQuery(query);
 
@@ -65,28 +88,41 @@ export default async function handler(
       const results = await Promise.race([executionPromise, timeoutPromise]);
 
       if (results && results.length > 0 && results[0].objects) {
-        console.log('Individual Objects:');
+        //console.log('Individual Objects:');
         const objectsArray = results[0].objects;
-        objectsArray.forEach((object: any, index: any) => {
-          console.log(`Object ${index + 1}:`, object);
-        });
-      } else {
+        let outputString = `You are now at ${StartPage} page and you want to go to ${GoalPage} webpage.`;
+
+        for (let i = 0; i < objectsArray.length; i++) {
+          const object = objectsArray[i];
+
+          //if (object.waypostDEMO && object.waypostDEMO[0]) {
+          //  outputString += ` First click on ${object.waypostDEMO[0]}.`;
+          //}
+
+          if (object.subLabel && object.label) {
+            outputString += ` Click ${object.subLabel}.`;
+          }
+
+          if (i === objectsArray.length - 1 && object.waypostDEMO && object.waypostDEMO[0]) {
+            outputString += ` You have arrived at the goal screen ${GoalPage}.`;
+          }
+        }
+
+        //console.log('outputstring output', outputString);
+        response.text = outputString;
+        //res.status(200).send(outputString);
+      } else if (StartPage == GoalPage) {
+        //console.log('You are already at the page where your information can be found.');
+        //res.status(200).json('You are already at the page where your information can be found.');
+        response.text = 'You are already at the page where your information can be found.'
+      }
+
+      else {
         console.log('No results found.');
       }
     } catch (error) {
       console.error('An error occurred:', error);
     }
-
-    /*
-    // method to extract and send metadata
-    response.sourceDocuments.forEach((document: any, index: any) => {
-      const source = document.metadata.source;
-      console.log(`Metadata for Document ${index + 1}:`, source);
-    });
-    */
-    const topDocument = response.sourceDocuments[0]; // Get the first document
-    const fileName = topDocument.metadata.source.split('\\').pop().replace('.pdf', ''); // Extract the file name without the extension
-    console.log('Top webpage name:', fileName);
 
 
 
